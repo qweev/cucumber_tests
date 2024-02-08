6703fb74480be609f56c2dcaaffed48b5370a143
qweev <qweev@o2.pl>
2024-02-08 16:52:46 +0100

    Add files via upload

--- src/test/java/wojtek/com/RunnerTest.java
+++ src/test/java/wojtek/com/RunnerTest.java
@@ -9,6 +9,8 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/wojtek/com/stories"},
         glue = {"wojtek.com"},
         plugin = {"pretty", "html:target/site/cucumber-pretty.html","summary" }
+//        tags = "@story1and2"
+        // can be run with filtering by tags, tags are set in feature files
 )
 public class RunnerTest {
     // you can run tests also from resources folders in IDE
